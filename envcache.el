88b1e3a42979ab744229414e46ffe0308563a6c3
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-04-06 22:01:29 +0900

    Add library header.

--- envcache.el
+++ envcache.el
@@ -1,3 +1,31 @@
+;;; envcache.el --- Fetch and cache shell environment variables.
+;;
+;; Filename: envcache.el
+;; Description: Fetch and cache shell environment variables.
+;; Author: Nobuo Saeki
+;; Maintainer: Nobuo Saeki
+;; Created: Mon Apr  6 21:56:58 2015 (+0900)
+;; Version: 0.0.1
+;; Package-Requires: ()
+;; URL: https://github.com/nsaeki/envcache.el
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
 (defvar envcache/env '())
 
 (defun envcache/init ()
